64c62056ff9e23fb331227aebe7e7a86cfa0303f
djay2806 <patildhananjay2806@gmail.com>
2026-06-12 00:21:42 +0530

    Add model and update app code

--- app.py
+++ app.py
@@ -2,8 +2,22 @@ import tensorflow as tf
 import streamlit as st
 from PIL import Image
 import numpy as np
+import os
+from huggingface_hub import hf_hub_download
 
-model = tf.keras.models.load_model("dog_cat_model.h5")
+model_path = "dog_cat_model.h5"
+
+if not os.path.exists(model_path):
+    with st.spinner("Downloading model from Hugging Face..."):
+        # This downloads the file from your public Hugging Face model repo
+        model_path = hf_hub_download(
+            repo_id="johnsnow321/CatvsDog-Classification", 
+            filename="dog_cat_model.h5"
+        )
+        st.success("Download complete!")
+
+model = tf.keras.models.load_model(model_path)
+
 
 st.title("Dog vs Cat Classifier")
 
